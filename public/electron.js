3bdfefdaf6c037602758bb4a2cf8b92e0cbd0bb3
laura <ilie_ioana_laura@yahoo.com>
2025-08-24 18:48:59 +0300

    closebutton event

--- public/electron.js
+++ public/electron.js
@@ -1,4 +1,5 @@
 const{app,BrowserWindow}=require('electron');
+const{ipcMain}=require('electron');
 const url=require('url');
 const path=require('path');
 
@@ -6,7 +7,14 @@ function createWindow(){
     const mainWindow=new BrowserWindow({
         title:"Pomodoro Timer",
         width:400,
-        height:400
+        height:400,
+        frame:false,
+        titleBarStyle: 'hidden',
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"), // Path to preload script
+            contextIsolation: true,   // Keeps context isolated for security
+            nodeIntegration: false,   // Disables Node.js in the renderer (security best practice)
+        }
     });
 
     const starturl=url.format({
@@ -14,6 +22,12 @@ function createWindow(){
      protocol:'file',
      slashes:true
     });
+    mainWindow.setWindowButtonVisibility(false);
+    mainWindow.setMenuBarVisibility(false);
     mainWindow.loadURL(starturl);//load in electron window
+    //close app event
+    ipcMain.on('close-app',()=>{
+        app.quit();
+    });
 }
 app.whenReady().then(createWindow);
